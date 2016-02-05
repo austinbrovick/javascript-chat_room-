f86b40e5feee100f2d5c8ebb1091fb746908f237
austinbrovick <austinbrovick@gmail.com>
2016-02-05 12:22:28 -0800

    commit with css

--- server.js
+++ server.js
@@ -5,7 +5,7 @@ var path = require("path");
 var app = express();
 
 // for static content
-app.use(express.static(path.join(__dirname, "./static")));
+app.use(express.static(path.join(__dirname, "/static")));
 // setting up ejs and our views folder
 app.set('views', path.join(__dirname, './views'));
 app.set('view engine', 'ejs');
